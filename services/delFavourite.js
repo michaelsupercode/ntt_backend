f9b66a192c9861b9d87a4109a2f76aa694f3abb6
Michael Hanel <michaelsupercode@gmail.com>
2024-04-26 13:29:35 +0200

    code

--- services/delFavourite.js
+++ services/delFavourite.js
@@ -1,1 +1,19 @@
 // mi_set
+const { _getDB } = require("../db_access/_getDB");
+const ObjectId = require("mongodb").ObjectId;
+
+async function deleteFavorite(userObjId, productObjId) {
+  console.log("", userObjId, productObjId);
+  const db = await _getDB();
+  const deleteFavorite = await db.collection("users").updateOne(
+    {
+      _id: new ObjectId(userObjId),
+    },
+    { $pull: { favorites: productObjId } }
+  );
+  return deleteFavorite;
+}
+
+module.exports = {
+  deleteFavorite,
+};
