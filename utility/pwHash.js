faf621b1297e4172cf5687cc6b081205d6cda9f3
Michael Hanel <michaelsupercode@gmail.com>
2024-04-26 13:22:39 +0200

    code

---
+++ utility/pwHash.js
@@ -0,0 +1,10 @@
+// mi_set
+const crypto = require("crypto");
+
+const hashPassword = (password) => {
+  return crypto.createHash("sha256").update(password, "utf8").digest("hex");
+};
+
+module.exports = {
+  hashPassword,
+};
---
+++ utility/verifyToken.js
@@ -0,0 +1,15 @@
+// mi_set
+const jwt = require("jsonwebtoken");
+const verifyToken = (token, newProduct) => {
+  jwt.verify(token, process.env.JWT_SECRET, (err, authorizedData) => {
+    if (err) {
+      console.log("ERROR: Could not connect to the protected route");
+      res.sendStatus(403);
+    } else {
+      console.log("SUCCESS: Connected to protected route");
+    }
+  });
+};
+module.exports = {
+  verifyToken,
+};
